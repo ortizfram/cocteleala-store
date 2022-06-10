f00dfb3171508420fa47c968455702d7f69f194d
melon <ortizfranco48@gmail.com>
2022-06-10 14:31:07 +0200

    js close payment button

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const wrapper = document.querySelector(".slider-wrapper");
 const menuItems = document.querySelectorAll(".menu-item");
 
-menuItems.forEach((item,index) => {
+menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
     });
 });
 
 
-const productButton = document.querySelector(".product-button");
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+    payment.style.display = "flex"
+})
+
+close.addEventListener("click", () => {
+    payment.style.display = "none"
+})
